4839378af3c54f96ab3f3657489de024a0a56c92
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-10 12:38:37 +0530

    Time: 6 ms (84.17%), Space: 43.3 MB (47.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,26 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-      Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
-                int val2 = stack.pop();
-                int val1 = stack.pop();
-                int ans = applyOperator(val1, val2, tokens[i]);
-                stack.push(ans);
-                continue;
-            }
-            stack.push(Integer.parseInt(tokens[i]));
-        }
-        return stack.peek();
-    }
+     Stack<Integer> s = new Stack<>();
+        Set<String> set = Set.of("+", "-", "*", "/");
 
-    public int applyOperator(int val1, int val2, String operator) {
-        switch (operator) {
-            case "+":
-                return val1 + val2;
-            case "-":
-                return val1 - val2;
-            case "*":
-                return val1 * val2;
-            case "/":
-                return val1 / val2;
+        for(String i : tokens)
+        {
+            if(set.contains(i))
+            {
+                int a = s.pop();
+                int b = s.pop();
+                int c = 0;
+                if(i.equals("+") ) c = b + a;
+                else if(i.equals("-") ) c = b - a;
+                else if(i.equals("*") ) c = b * a;
+                else c = b / a;
+
+                s.push(c);
+            }
+            else {
+                s.push(Integer.parseInt(i));
+            }
         }
-        return 0;
+        return s.peek();
     }
 }
