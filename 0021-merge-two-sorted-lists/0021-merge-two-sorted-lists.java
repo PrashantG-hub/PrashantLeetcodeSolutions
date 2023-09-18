38ee92671531e80d735cfa2a64fcfb260d2c0450
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-18 21:18:55 +0530

    Time: 0 ms (100%), Space: 41.6 MB (28.86%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,28 +10,56 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+// --------------WITH SPACE COMPLEXITY AS O(1)--------------------------------------
+if(list1==null)
+return list2;
 
-       ListNode result = new ListNode(0);
-       ListNode temp=result;
-        ListNode t1=list1,t2=list2;
-      while(t1!=null && t2!=null){
-       if(t1.val<=t2.val){
-        temp.next=new ListNode(t1.val);
-        temp=temp.next;
-        t1=t1.next;
-      }
-      else{
-       temp.next=new ListNode(t2.val);
-        temp=temp.next;
-        t2=t2.next;
-      }
-      }
-      if(t1 != null) {
-            temp.next = t1;
-        } else {
-            temp.next = t2;
-        }
-      return result.next;
+if(list2==null)
+return list1;
+ 
+ if(list1.val>list2.val){
+     ListNode temp1=list1;
+     list1=list2;
+     list2=temp1;
+ }
+ ListNode res=list1;
+while(list1!=null && list2!=null){
+    ListNode temp=null;
+    while(list1!=null && list1.val<=list2.val){
+       temp=list1;
+       list1=list1.next;
+    }
+    temp.next=list2;
+
+    ListNode temp1=list1;
+     list1=list2;
+     list2=temp1;
+}
+return res;
+// --------------WITH SPACE COMPLEXITY AS O(M+N)-------------------------------------
+
+
+    //    ListNode result = new ListNode(0);
+    //    ListNode temp=result;
+    //     ListNode t1=list1,t2=list2;
+    //   while(t1!=null && t2!=null){
+    //    if(t1.val<=t2.val){
+    //     temp.next=new ListNode(t1.val);
+    //     temp=temp.next;
+    //     t1=t1.next;
+    //   }
+    //   else{
+    //    temp.next=new ListNode(t2.val);
+    //     temp=temp.next;
+    //     t2=t2.next;
+    //   }
+    //   }
+    //   if(t1 != null) {
+    //         temp.next = t1;
+    //     } else {
+    //         temp.next = t2;
+    //     }
+    //   return result.next;
      }
 
 }
