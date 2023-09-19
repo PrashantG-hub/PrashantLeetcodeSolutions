2ee2ddeb0415e6a50e950d7790c561b3002052e6
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-19 23:33:06 +0530

    Time: 1 ms (100%), Space: 44.4 MB (52.11%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -19,13 +19,16 @@ class Solution {
         if(root==null)
         return sum;
 
-        if(root == null) return sum;
         if(root.val % 2 == 0)
         {
-            if(root.left != null && root.left.left != null) sum += root.left.left.val;
-            if(root.left != null && root.left.right != null) sum += root.left.right.val;
-            if(root.right != null && root.right.left != null) sum += root.right.left.val;
-            if(root.right != null && root.right.right != null) sum += root.right.right.val;
+             if(root.left!=null && root.left.left!=null)
+            sum+=root.left.left.val;
+            if(root.left!=null && root.left.right!=null)
+            sum+=root.left.right.val;
+            if(root.right!=null && root.right.left!=null)
+            sum+=root.right.left.val;
+            if(root.right!=null && root.right.right!=null)
+            sum+=root.right.right.val;
         }
         sumEvenGrandparent(root.left);
         sumEvenGrandparent(root.right);
