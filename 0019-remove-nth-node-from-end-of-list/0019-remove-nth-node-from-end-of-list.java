c05f6520d9e1901c4fadad83b3944d083b464a56
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-19 12:11:01 +0530

    Time: 0 ms (100%), Space: 40.2 MB (93.39%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,22 +10,39 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size=1;
-        ListNode l1=head;
-        while(l1.next!=null){
-            l1=l1.next;
-            size++;
-        }
 
-          if(size==n){
-            head=head.next;
-            return head;
-        }
-        ListNode l2=head;
-        for(int i=1;i<size-n;i++){
-            l2=l2.next;
-        }
-        l2.next=l2.next.next;
-        return head;
+// Time Complexity O(2N)~O(N)  -------Space complexity-O(1)----------------------------------------
+ListNode Start=new ListNode();
+Start.next=head;
+ListNode fast=Start;
+for(int i=1;i<=n;i++){
+    fast=fast.next; 
+}
+ListNode slow=Start;
+while(fast.next!=null){
+    fast=fast.next;
+    slow=slow.next;
+}
+slow.next=slow.next.next;
+return Start.next;
+
+// Time Complexity O(2N)~O(N)  -------Space complexity-O(1)----------------------------------------
+        // int size=1;
+        // ListNode l1=head;
+        // while(l1.next!=null){
+        //     l1=l1.next;
+        //     size++;
+        // }
+
+        //   if(size==n){
+        //     head=head.next;
+        //     return head;
+        // }
+        // ListNode l2=head;
+        // for(int i=1;i<size-n;i++){
+        //     l2=l2.next;
+        // }
+        // l2.next=l2.next.next;
+        // return head;
     }
 }
