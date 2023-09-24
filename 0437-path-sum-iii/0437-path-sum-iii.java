c1b6c32cf1f6132183f33c5358cc6e160a13512b
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-24 21:04:59 +0530

    Time: 2 ms (100%), Space: 43.8 MB (8.16%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,32 +15,66 @@
  */
 class Solution {
     int allcomb=0;
-    public int pathSum(TreeNode root, int targetSum) {
+
+     public int pathSum(TreeNode root, int targetSum) {
         if(root==null)
         return allcomb;
+      
+      HashMap<Long, Integer> hm=new HashMap<>();
+      hm.put((long)0,1);
 
-        findallsum(root,targetSum,0);
-
-         pathSum(root.left,targetSum);
-         pathSum(root.right,targetSum);
+        findallsum(root,targetSum,0,hm);
 
         return allcomb;
     }
-   
-   void findallsum(TreeNode root, int targetSum,long sum){
+
+   void findallsum(TreeNode root, int targetSum,long sum,HashMap<Long, Integer> hm){
      
      if(root==null)
      return;
 
      sum+=root.val;
-    
-     if(sum==targetSum)
-     allcomb++;
+    if(hm.containsKey(sum-targetSum))
+    allcomb+=hm.get(sum-targetSum);
 
-     findallsum(root.left,targetSum,sum);
-     findallsum(root.right,targetSum,sum);
+    hm.put(sum,hm.getOrDefault(sum,0)+1);
+
+     findallsum(root.left,targetSum,sum,hm);
+     findallsum(root.right,targetSum,sum,hm);
+     hm.put(sum,hm.get(sum)-1);
+    
+   }
+
+
+    //--------- TC O(N), SC O(1)-Fixing one Node and travesing along the tree-------------------------------------------
+
+
+//     public int pathSum(TreeNode root, int targetSum) {
+//         if(root==null)
+//         return allcomb;
+
+//         findallsum(root,targetSum,0);
+
+//          pathSum(root.left,targetSum);
+//          pathSum(root.right,targetSum);
+
+//         return allcomb;
+//     }
+   
+//    void findallsum(TreeNode root, int targetSum,long sum){
+     
+//      if(root==null)
+//      return;
+
+//      sum+=root.val;
+    
+//      if(sum==targetSum)
+//      allcomb++;
+
+//      findallsum(root.left,targetSum,sum);
+//      findallsum(root.right,targetSum,sum);
      
 
-   }
+//    }
 
 }
