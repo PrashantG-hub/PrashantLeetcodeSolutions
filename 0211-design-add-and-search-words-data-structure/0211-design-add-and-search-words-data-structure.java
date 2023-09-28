0759f8455a1ab60be9138d197c3208742f462e3b
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2023-09-28 15:58:07 +0530

    Time: 199 ms (53.64%), Space: 92 MB (82.14%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,66 @@
+class TrieNode{
+       TrieNode[] children;
+       boolean isEndOfWord;
+
+       public TrieNode(){
+           children=new TrieNode[26];
+           for(int i=0;i<26;i++)
+           children[i]=null;
+
+           isEndOfWord=false;
+       }
+}
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        root=new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            int idx=word.charAt(i)-'a';
+            if(curr.children[idx]==null)
+            curr.children[idx]=new TrieNode();
+
+            if(i==word.length()-1)
+            curr.children[idx].isEndOfWord=true;
+
+            curr=curr.children[idx];
+        }
+    }
+    
+    public boolean search(String word) {
+        return SearchHelper(root,word,0);
+    }
+    public boolean SearchHelper(TrieNode curr,String word,int idx){
+
+          if(idx==word.length())
+            return  curr.isEndOfWord;
+            
+            if(word.charAt(idx)=='.')
+            {
+                for(TrieNode child:curr.children){
+                  if(child!=null && SearchHelper(child,word,idx+1))
+                  return true;
+                }
+                 return false;
+            }    
+            else
+            {
+            TrieNode child=curr.children[word.charAt(idx)-'a'];
+            if(child==null)
+            return false;
+
+            return SearchHelper(child,word,idx+1);
+            }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
