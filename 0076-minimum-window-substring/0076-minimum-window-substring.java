c3bc3c713c18418ea080786029913080106ae3a5
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2024-06-08 14:02:20 +0530

    Time: 17 ms (56.96%), Space: 44.5 MB (84.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,16 +1,16 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n=s.length();
 
-        if (s == null || t == null || n == 0 || t.length() == 0 || n < t.length()) 
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) 
             return "";
 
         HashMap<Character,Integer> hm=new HashMap<>();
         for(int i=0;i<t.length();i++)
             hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)+1);
         
-        return FindMinimumSubString(hm,n,s,t);
+        return FindMinimumSubString(hm,s.length(),s,t);
     }
+    
         String FindMinimumSubString(HashMap<Character,Integer> hm, int n,String s,String t){
           int j=0,i=0,count=t.length(),minstart=0,minwindow=Integer.MAX_VALUE;
         String result="";
