9914762a23ef06f8c24a86d2f354892448b59b90
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-01 23:47:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.java
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -39,23 +39,17 @@ class Solution
 {
   public void insertionSort(int arr[], int n)
   {
-  if (n <= 1) {
-            return;
-        }
-
-        // Sort the first n-1 elements
-        insertionSort(arr, n - 1);
-
-        // Insert the nth element into the sorted array of size n-1
-        int last = arr[n - 1];
-        int j = n - 2;
-
-        // Move elements of arr[0..n-1], that are greater than last,
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > last) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = last;
+   if(n<=1)
+   return;
+   
+   insertionSort(arr,n-1);
+   int end=arr[n-1];
+   int j=n-2;
+   
+   while(j>=0 && arr[j]>end){
+       arr[j+1]=arr[j];
+       j--;
+   }
+   arr[j+1]=end;
   }
 }
