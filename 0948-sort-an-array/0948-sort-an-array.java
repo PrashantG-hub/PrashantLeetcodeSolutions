dcfeb49535c4b8009f3ef0bfcd24a71c1acebb96
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-05 15:55:32 +0530

    Time: 25 ms (62.36%), Space: 56 MB (54.43%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+      mergeSort(arr,0,arr.length-1);
+      return arr;
+        
+	}
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int middle = left + (right - left) / 2;
+
+            // Recursively sort first and second halves
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int middle, int right) {
+        // Sizes of the two subarrays
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Temporary arrays to hold the values
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(arr, left, leftArray, 0, n1);
+        System.arraycopy(arr, middle + 1, rightArray, 0, n2);
+
+        // Initial indexes of subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[], if any
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[], if any
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    }
