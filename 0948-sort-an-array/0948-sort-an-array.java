d09ae6aaf908e2c39732890bff9c9b46631c823e
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-06 11:45:58 +0530

    Time: 26 ms (54.39%), Space: 56 MB (58.59%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,69 +1,54 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-        mergeSort(arr, 0, arr.length - 1);
+        mergeSort(arr,0,arr.length-1);
         return arr;
-
     }
 
-    private static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            // Find the middle point
-            int middle = left + (right - left) / 2;
+    private static void mergeSort(int[] arr,int left,int right){
+        if(left<right){
+            int middle=left+(right-left)/2;
+            mergeSort(arr,left,middle);
+            mergeSort(arr,middle+1,right);
 
-            // Recursively sort first and second halves
-            mergeSort(arr, left, middle);
-            mergeSort(arr, middle + 1, right);
-
-            // Merge the sorted halves
-            merge(arr, left, middle, right);
+            merge(arr,left,middle,right);
         }
     }
 
-    private static void merge(int[] arr, int left, int middle, int right) {
-        // Sizes of the two subarrays
-        int n1 = middle - left + 1;
-        int n2 = right - middle;
+   private static void merge(int[] arr, int left, int middle, int right){
+        int n1=middle+1-left;
+        int n2=right-middle;
 
-        // Temporary arrays to hold the values
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
+        int[] leftarray=new int[n1];
+        int[] rightarray=new int[n2];
 
-        // Copy data to temporary arrays
-        
-        for(int i=0;i<n1;i++)
-        leftArray[i]=arr[left+i];
-        //System.arraycopy(arr, left, leftArray, 0, n1);
-        System.arraycopy(arr, middle + 1, rightArray, 0, n2);
+        System.arraycopy(arr,left,leftarray,0,n1);
+        System.arraycopy(arr,middle+1,rightarray,0,n2);
 
-        // Initial indexes of subarrays
-        int i = 0, j = 0;
+        int i=0,j=0;
+        int k=left;
 
-        // Initial index of merged subarray
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                arr[k] = leftArray[i];
+        while(i<n1 && j<n2){
+            if(leftarray[i]<=rightarray[j]){
+                arr[k]=leftarray[i];
                 i++;
-            } else {
-                arr[k] = rightArray[j];
+            }
+            else{
+                arr[k]=rightarray[j];
                 j++;
             }
             k++;
         }
 
-        // Copy remaining elements of leftArray[], if any
-        while (i < n1) {
-            arr[k] = leftArray[i];
+        while(i<n1){
+            arr[k]=leftarray[i];
             i++;
             k++;
         }
 
-        // Copy remaining elements of rightArray[], if any
-        while (j < n2) {
-            arr[k] = rightArray[j];
+        while(j<n2){
+            arr[k]=rightarray[j];
             j++;
             k++;
         }
     }
-
 }
