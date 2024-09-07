cd61dd9264fea2d04f79a3651740fb68fb8b65b0
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-07 12:10:36 +0530

    Time: 1588 ms (5%), Space: 59.5 MB (5.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -6,29 +6,23 @@ class Solution {
     public void quickSort(int[] arr,int i, int j){
        if (i < j) {
 			int p = partition(arr, i, j); 
-            // p is fixed and call quickSort again for left and right array
+         
 			quickSort(arr, i, p-1); 
 			quickSort(arr, p+1, j); 
 		}
     }
     public int partition(int[] arr,int l,int h) {
-        // In worst case it will be o(n^2). i.e. if data is already sorted
-		// we can improve it by using middle element as pivot and then before starting
-		// anything just swap it with h. so, that h will remain pivot.
-
-		// Find mid of the array
+        
 		int mid = (l + h) / 2;
-		// swap array element present in mid and h index
+		
 		swapIntArray(arr, mid, h);
-		// now pivot is h again.
-		// we goes from l till h-1 because h is reserved for pivot
+	
 		for (int j = l; j < h; j++) {
 			if (arr[j] < arr[h]) {
 				swapIntArray(arr, j, l);
 				l++;
 			}
 		}
-		// when j reached h,we know that l is in pivot position. so we just swap it
 		swapIntArray(arr, h, l);
 		return l;
     }
