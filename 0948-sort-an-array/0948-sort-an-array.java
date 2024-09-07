2d04d9885e6429cdd7bf27c0ab5d09d7ce0c5cd4
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-07 15:35:06 +0530

    Time: 281 ms (6.59%), Space: 58.2 MB (5.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,35 +1,37 @@
 class Solution {
-    static void quickSort(int[] nums, int low, int high) {
-        if (low >= high) {
-            return;
-        }
+    public int[] sortArray(int[] nums) {
+        QuickSort(nums,0,nums.length-1);
+        return nums;
 
-        int start = low;
-        int end = high;
-        int mid = start + (end - start) / 2;
-        int pivot = nums[mid];
-
-        while (start <= end) {
-            while (nums[start] < pivot) {
-                start++;
-            }
-            while (nums[end] > pivot) {
-                end--;
-            }
-            if (start <= end) {
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
-            }
-        }
-        quickSort(nums, low, end);
-        quickSort(nums, start, high);
     }
 
-    public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-        return nums;
+    public static void QuickSort(int[] nums, int low, int high){
+        if(low>=high)
+        return;
+
+        int start=low;
+        int end=high;
+        int mid=start+(end-start)/2;
+        int pivot=nums[mid];
+
+        while(start<=end){
+
+            while(nums[start]<pivot)
+            start++;
+
+            while(nums[end]>pivot)
+            end--;
+
+            if(start<=end){
+                int temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                start++;
+                end--;
+            
+            }
+        }
+        QuickSort(nums,low,end);
+        QuickSort(nums,start, high);
     }
 }
