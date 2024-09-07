5ea6c4728cdcd5305b32d92b081b3676fa27ba81
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-09-07 12:48:32 +0530

    Time: 278 ms (7.64%), Space: 58.1 MB (5.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,34 +1,35 @@
 class Solution {
+    static void quickSort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+
+        while (start <= end) {
+            while (nums[start] < pivot) {
+                start++;
+            }
+            while (nums[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quickSort(nums, low, end);
+        quickSort(nums, start, high);
+    }
+
     public int[] sortArray(int[] nums) {
         quickSort(nums, 0, nums.length - 1);
         return nums;
-    }
-    public void quickSort(int[] arr,int i, int j){
-       if (i < j) {
-			int p = partition(arr, i, j); 
-         
-			quickSort(arr, i, p-1); 
-			quickSort(arr, p+1, j); 
-		}
-    }
-    public int partition(int[] arr,int l,int h) {
-        
-		int mid = (l + h) / 2;
-		
-		swapIntArray(arr, mid, h);
-	
-		for (int j = l; j < h; j++) {
-			if (arr[j] < arr[h]) {
-				swapIntArray(arr, j, l);
-				l++;
-			}
-		}
-		swapIntArray(arr, h, l);
-		return l;
-    }
-    private void swapIntArray(int[] a, int i,int j){
-        int temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
     }
 }
